216d27b1007a1104ed0376a3640369b91c424238
MercenaryCarter <30935189+MercenaryCarter@users.noreply.github.com>
2017-08-11 14:06:28 -0400

    Sample PHP Code
    Showcases how users can query the full list of their projects within SERPWoo

---
+++ list-all-projects.php
@@ -0,0 +1,26 @@
+<?php
+
+//This code requests all of your projects and their details in JSON format
+
+
+	// Get your API Key here: https://www.serpwoo.com/v3/api/
+		$API_key = "API_KEY_HERE";
+
+		$requestURL = "https://api.serpwoo.com/v1/projects/?key=" . $API_key;
+
+
+	// Use Curl to get the request
+		$options = array(
+			CURLOPT_RETURNTRANSFER => true
+		);
+
+		$ch = curl_init($requestUrl);
+		curl_setopt_array($ch, $options);
+
+		$json_content = curl_exec($ch);
+		curl_close($ch);
+
+
+	//Print Result
+		print_r($json_content);
+?>
