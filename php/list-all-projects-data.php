c8a8bfcade84d77720dd65fc6f4a50a84554fb89
CCarter <30935189+MercenaryCarter@users.noreply.github.com>
2017-08-15 13:09:27 -0400

    split the project script so it makes more sense
    Re-did the script and separated the JSON mass data output to a different file, and now have an html output of the data for list-all-projects.php. The HTML output in the list-all-projects.php file now outputs links to each projects' SEO keywords.

---
+++ php/list-all-projects-data.php
@@ -0,0 +1,31 @@
+<?php
+//
+// This code requests all of your projects' data in JSON format
+//
+// Last updated - Aug 15th, 2017 @ 12:58 PM EST (@MercenaryCarter https://github.com/MercenaryCarter and https://twitter.com/MercenaryCarter)
+//
+
+	//outputs data in JSON format
+		header('Content-type: application/json');
+
+	// Get your API Key here: https://www.serpwoo.com/v3/api/ (should be logged in)
+		$API_key = "API_KEY_HERE";
+		$requestURL = "https://api.serpwoo.com/v1/projects/?key=" . $API_key;
+
+	// Use Curl to get the request
+		$options = array(
+			CURLOPT_RETURNTRANSFER => true
+		);
+
+		$ch = curl_init($requestURL);
+		curl_setopt_array($ch, $options);
+
+		$json_content = curl_exec($ch);
+		curl_close($ch);
+
+	//outputs data in JSON format
+		header('Content-type: application/json');
+
+	//Print Result
+		print_r($json_content);
+?>
