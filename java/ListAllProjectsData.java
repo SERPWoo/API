945412766c3f87742b42ff9b1b82deb5ebb8c081
CCarter <30935189+MercenaryCarter@users.noreply.github.com>
2017-08-20 12:06:17 -0400

    Added Java sample code
    Added Java sample code that outputs the "list-all-projects-data" variation.

---
+++ java/ListAllProjectsData.java
@@ -0,0 +1,60 @@
+//
+// This code requests all of your projects' data in JSON format
+//
+// Last updated - Aug 20th, 2017 @ 12:05 EST (@MercenaryCarter https://github.com/MercenaryCarter and https://twitter.com/MercenaryCarter)
+//
+// Compile Command: javac ListAllProjectsData.java
+// Run Command: java ListAllProjectsData
+//
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * A complete Java class that demonstrates how to read content (text) from a URL
+ * using the Java URL and URLConnection classes.
+ * @author alvin alexander, alvinalexander.com
+ */
+ 
+public class ListAllProjectsData
+{
+  public static void main(String[] args)
+  {
+  	String API_key = "API_KEY_HERE";
+    String output  = getUrlContents("https://api.serpwoo.com/v1/projects/?key=" + API_key);
+    System.out.println(output);
+  }
+
+  private static String getUrlContents(String theUrl)
+  {
+    StringBuilder content = new StringBuilder();
+
+    // many of these calls can throw exceptions, so i've just
+    // wrapped them all in one try/catch statement.
+    try
+    {
+      // create a url object
+      URL url = new URL(theUrl);
+
+      // create a urlconnection object
+      URLConnection urlConnection = url.openConnection();
+
+      // wrap the urlconnection in a bufferedreader
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+      String line;
+
+      // read from the urlconnection via the bufferedreader
+      while ((line = bufferedReader.readLine()) != null)
+      {
+        content.append(line + "\n");
+      }
+      bufferedReader.close();
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+    return content.toString();
+  }
+}
